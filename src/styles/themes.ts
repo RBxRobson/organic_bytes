9481f08b1aaa1f83deb542daab5034835a81bf23
RBxRobson <robsonnbarbosa0@gmail.com>
2024-09-19 17:15:59 -0300

    feat: Adiciona as variáveis de cores

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,11 +1,34 @@
 export default {
   colors: {
-    primary: 'tomato',
-    white: '#fff'
-  },
-  spacings: {
-    small: '1rem',
-    medium: '2rem',
-    large: '3rem'
+    softPrimary: '#84D187',
+    primary: '#00B207',
+    hardPrimary: '#2C742F',
+    warning: '#FF8A00',
+    danger: '#EA4B48',
+    white: '#FFFFFF',
+    darkShades: {
+      d_9: '#191919',
+      d_8: '#333333',
+      d_7: '#4D4D4D',
+      d_6: '#666666',
+      d_5: '#808080',
+      d_4: '#999999',
+      d_3: '#B3B3B3',
+      d_2: '#CCCCCC',
+      d_1: '#E6E6E6',
+      d_0: '#F2F2F2'
+    },
+    greenShades: {
+      g_9: '#002603',
+      g_8: '#173B1A',
+      g_7: '#2B572E',
+      g_6: '#406B42',
+      g_5: '#618062',
+      g_4: '#7A997C',
+      g_3: '#96B297',
+      g_2: '#B4CCB4',
+      g_1: '#DAE5DA',
+      g_0: '#EDF2EE'
+    }
   }
 }
