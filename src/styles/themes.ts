b857ac96bc743acbe29c9987483c60b40de20665
RBxRobson <robsonnbarbosa0@gmail.com>
2024-09-20 04:40:08 -0300

    feat: Adiciona estrutura hierárquica para variáveis de tipografia

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,34 +1,69 @@
-export default {
+const theme = {
   colors: {
-    softPrimary: '#84D187',
-    primary: '#00B207',
-    hardPrimary: '#2C742F',
+    primary: {
+      soft: '#84D187',
+      normal: '#00B207',
+      hard: '#2C742F'
+    },
     warning: '#FF8A00',
     danger: '#EA4B48',
-    white: '#FFFFFF',
+    neutral: {
+      white: '#FFFFFF',
+      black: '#000'
+    },
     darkShades: {
-      d_9: '#191919',
-      d_8: '#333333',
-      d_7: '#4D4D4D',
-      d_6: '#666666',
-      d_5: '#808080',
-      d_4: '#999999',
-      d_3: '#B3B3B3',
-      d_2: '#CCCCCC',
-      d_1: '#E6E6E6',
-      d_0: '#F2F2F2'
+      d9: '#191919',
+      d8: '#333333',
+      d7: '#4D4D4D',
+      d6: '#666666',
+      d5: '#808080',
+      d4: '#999999',
+      d3: '#B3B3B3',
+      d2: '#CCCCCC',
+      d1: '#E6E6E6',
+      d0: '#F2F2F2'
     },
     greenShades: {
-      g_9: '#002603',
-      g_8: '#173B1A',
-      g_7: '#2B572E',
-      g_6: '#406B42',
-      g_5: '#618062',
-      g_4: '#7A997C',
-      g_3: '#96B297',
-      g_2: '#B4CCB4',
-      g_1: '#DAE5DA',
-      g_0: '#EDF2EE'
+      g9: '#002603',
+      g8: '#173B1A',
+      g7: '#2B572E',
+      g6: '#406B42',
+      g5: '#618062',
+      g4: '#7A997C',
+      g3: '#96B297',
+      g2: '#B4CCB4',
+      g1: '#DAE5DA',
+      g0: '#EDF2EE'
+    }
+  },
+
+  typography: {
+    fontSize: {
+      heading: {
+        xxl: '72px',
+        xl: '56px',
+        large: '48px',
+        medium: '40px',
+        small: '36px',
+        tiny: '32px'
+      },
+      body: {
+        xxl: '24px',
+        xl: '20px',
+        large: '18px',
+        medium: '16px',
+        small: '14px',
+        tiny: '12px'
+      }
+    },
+    fontWeight: {
+      light: 300,
+      regular: 400,
+      medium: 500,
+      semiBold: 600,
+      bold: 700
     }
   }
 }
+
+export default theme
