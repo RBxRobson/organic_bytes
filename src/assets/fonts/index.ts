acea2b3d057061f783584bf9b8e53dd8ffa00c91
RBxRobson <robsonnbarbosa0@gmail.com>
2024-09-20 04:36:26 -0300

    feat: Adiciona arquivos de fonte

---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,7 @@
+import PPBold from './Poppins-Bold.ttf'
+import PPLight from './Poppins-Light.ttf'
+import PPMedium from './Poppins-Medium.ttf'
+import PPRegular from './Poppins-Regular.ttf'
+import PPSemiBold from './Poppins-SemiBold.ttf'
+
+export { PPBold, PPLight, PPMedium, PPRegular, PPSemiBold }
