98ea007b6dbd649bbc482860893e2c4d9bddb649
RBxRobson <robsonnbarbosa0@gmail.com>
2024-09-21 09:36:03 -0300

    feat: Implementa as fontes no estilo global

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,10 +1,33 @@
 import { createGlobalStyle } from 'styled-components'
 
+import {
+  PPBold,
+  PPLight,
+  PPMedium,
+  PPRegular,
+  PPSemiBold
+} from '../assets/fonts'
+
+const fontFace = (src: string, weight = 400) => `
+  @font-face {
+    font-family: 'Poppins';
+    src: url(${src});
+    font-weight: ${weight};
+  }
+`
+
 const GlobalStyles = createGlobalStyle`
+  ${fontFace(PPLight, 300)}
+  ${fontFace(PPRegular, 400)}
+  ${fontFace(PPMedium, 500)}
+  ${fontFace(PPSemiBold, 600)}
+  ${fontFace(PPBold, 700)}
+
   html,
   body {
     padding: 0;
     margin: 0;
+    font-family: 'Poppins', sans-serif;
   }
 
   a {
